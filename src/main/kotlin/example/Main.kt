a8b013314a074bf48daea516eb180ea877617011
Brian Henning <henninb@msn.com>
2021-08-12 06:57:31 -0500

    initial version

---
+++ src/main/kotlin/example/Main.kt
@@ -0,0 +1,49 @@
+//@file:Suppress("DEPRECATION", "UNUSED")
+
+package example
+
+
+import example.config.cors
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import org.slf4j.event.Level
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+
+    install(DefaultHeaders)
+    install(CORS) {
+        cors()
+    }
+
+    install(CallLogging) {
+        level = Level.INFO
+    }
+
+//    install(Authentication) {
+//        jwt()
+//    }
+//
+//    install(ContentNegotiation) {
+//        jackson {
+//            enable(SerializationFeature.INDENT_OUTPUT)
+//        }
+//    }
+
+    //DatabaseFactory.init()
+
+    routing {
+
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+
+        /*install(StatusPages) {
+            statusPages()
+        }*/
+       // api()
+    }
+}
---
+++ src/main/kotlin/example/config/Cors.kt
@@ -0,0 +1,25 @@
+package example.config
+
+
+import io.ktor.features.CORS
+import io.ktor.features.maxAgeDuration
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import kotlin.time.ExperimentalTime
+import kotlin.time.days
+
+//@OptIn(ExperimentalTime::class)
+fun CORS.Configuration.cors() {
+    method(HttpMethod.Options)
+    method(HttpMethod.Get)
+    method(HttpMethod.Post)
+    method(HttpMethod.Put)
+    method(HttpMethod.Delete)
+    header(HttpHeaders.AccessControlAllowHeaders)
+    header(HttpHeaders.AccessControlAllowOrigin)
+    header(HttpHeaders.Authorization)
+    allowCredentials = true
+    allowSameOrigin = true
+    anyHost()
+    //maxAgeDuration = 1.days
+}
